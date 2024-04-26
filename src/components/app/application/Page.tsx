de99e4e3062e19eced8b079c47c5b8c2fde87712
BenDaye <yypsgdsg@163.com>
2024-04-27 01:05:20 +0800

    feat: app application page

--- src/components/app/application/Page.tsx
+++ src/components/app/application/Page.tsx
@@ -3,18 +3,30 @@ import { FallbackId } from '@/constants/common';
 import {
   UseAppApplicationHookDataSchema,
   useAppApplication,
+  useAppUser,
   useDashboardApplicationAsset,
 } from '@/hooks';
 import { OverridesProps } from '@/types/overrides';
+import { ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionDetailsProps,
+  AccordionProps,
+  AccordionSummary,
+  AccordionSummaryProps,
   Avatar,
+  AvatarGroup,
+  AvatarProps,
   Box,
   Card,
   CardHeader,
   Chip,
   Divider,
+  List,
+  ListItem,
+  ListItemText,
   Stack,
-  StackProps,
   Tab,
   Tabs,
   Typography,
@@ -176,7 +188,7 @@ const ApplicationPageSecondaryContent = ({
   return (
     <Box>
       <Box sx={{ height: 48 }} />
-      <Stack gap={2} sx={{ px: 2, pb: 2 }}>
+      <Stack sx={{ px: 1, pb: 2 }}>
         <ApplicationPageSecondaryContentSection
           title={t('application:Category._')}
         >
@@ -185,11 +197,66 @@ const ApplicationPageSecondaryContent = ({
             label={t(`application:Category.Name.${data.category}`)}
           />
         </ApplicationPageSecondaryContentSection>
-        <ApplicationPageSecondaryContentSection title={t('application:Tag._')}>
+        <ApplicationPageSecondaryContentSection
+          title={t('application:Tag.Tags')}
+        >
           {data.tags.length ? (
-            data.tags.map((tag) => (
-              <Chip key={tag.id} variant="outlined" label={tag.name} />
+            data.tags.map((item) => (
+              <Chip key={item.id} variant="outlined" label={item.name} />
             ))
+          ) : (
+            <EmptyDataBox height={32} />
+          )}
+        </ApplicationPageSecondaryContentSection>
+        <ApplicationPageSecondaryContentSection
+          title={t('application:Information.Information')}
+        >
+          <ApplicationPageSecondaryContentInformationList data={data} />
+        </ApplicationPageSecondaryContentSection>
+        <ApplicationPageSecondaryContentSection
+          title={t('application:Group.Groups')}
+        >
+          {data.groups.length ? (
+            data.groups.map((item) => (
+              <Chip key={item.id} variant="outlined" label={item.name} />
+            ))
+          ) : (
+            <EmptyDataBox height={32} />
+          )}
+        </ApplicationPageSecondaryContentSection>
+        <ApplicationPageSecondaryContentSection
+          title={t('application:Collection.Collections')}
+        >
+          {data.collections.length ? (
+            data.collections.map((item) => (
+              <Chip key={item.id} variant="outlined" label={item.name} />
+            ))
+          ) : (
+            <EmptyDataBox height={32} />
+          )}
+        </ApplicationPageSecondaryContentSection>
+        <ApplicationPageSecondaryContentSection
+          title={t('application:Follow.Followers')}
+        >
+          {data.followers.length ? (
+            <AvatarGroup variant="rounded" total={data.followers.length}>
+              {data.followers.map((item) => (
+                <UserAvatar key={item.userId} userId={item.userId} />
+              ))}
+            </AvatarGroup>
+          ) : (
+            <EmptyDataBox height={32} />
+          )}
+        </ApplicationPageSecondaryContentSection>
+        <ApplicationPageSecondaryContentSection
+          title={t('application:Own.Owners')}
+        >
+          {data.owners.length ? (
+            <AvatarGroup variant="rounded" total={data.owners.length}>
+              {data.owners.map((item) => (
+                <UserAvatar key={item.userId} userId={item.userId} />
+              ))}
+            </AvatarGroup>
           ) : (
             <EmptyDataBox height={32} />
           )}
@@ -199,21 +266,177 @@ const ApplicationPageSecondaryContent = ({
   );
 };
 
+type ApplicationPageUserAvatarProps = OverridesProps<{
+  AvatarProps?: AvatarProps;
+}> & {
+  userId: string;
+};
+const UserAvatar = ({ overrides, userId }: ApplicationPageUserAvatarProps) => {
+  const {
+    data: { avatarText, avatarSrc },
+  } = useAppUser(userId);
+  return (
+    <Avatar src={avatarSrc || undefined} {...overrides?.AvatarProps}>
+      {avatarText}
+    </Avatar>
+  );
+};
+
 type ApplicationPageSecondaryContentSectionProps = PropsWithChildren<
-  OverridesProps<{ WrapperProps?: StackProps }> & {
+  OverridesProps<{
+    WrapperProps?: Omit<AccordionProps, 'children'>;
+    HeaderProps?: Omit<AccordionSummaryProps, 'children'>;
+    ContentProps?: Omit<AccordionDetailsProps, 'children'>;
+  }> & {
     title: string;
+    defaultExpanded?: AccordionProps['defaultExpanded'];
   }
 >;
 const ApplicationPageSecondaryContentSection = ({
   overrides,
   title,
+  defaultExpanded = true,
   children,
 }: ApplicationPageSecondaryContentSectionProps) => {
   return (
-    <Stack gap={1} {...overrides?.WrapperProps}>
-      <Typography variant="subtitle2">{title}</Typography>
-      <Divider flexItem />
-      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>{children}</Box>
-    </Stack>
+    <Accordion
+      defaultExpanded={defaultExpanded}
+      variant="outlined"
+      disableGutters
+      {...overrides?.WrapperProps}
+    >
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        {...overrides?.HeaderProps}
+      >
+        {title}
+      </AccordionSummary>
+      <AccordionDetails {...overrides?.ContentProps}>
+        {children}
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+const ApplicationPageSecondaryContentInformationList = ({
+  data,
+}: {
+  data: UseAppApplicationHookDataSchema;
+}) => {
+  const { t } = useTranslation();
+  return (
+    <List disablePadding dense>
+      <ListItem disablePadding divider>
+        <ListItemText
+          primary={t('application:Information.Website')}
+          primaryTypographyProps={{
+            variant: 'caption',
+            color: 'text.secondary',
+          }}
+          secondary={data.website}
+          secondaryTypographyProps={{
+            variant: 'body2',
+            noWrap: true,
+            textOverflow: 'ellipsis',
+            color: 'text.primary',
+          }}
+        />
+      </ListItem>
+      <ListItem disablePadding divider>
+        <ListItemText
+          primary={t('application:Information.Github')}
+          primaryTypographyProps={{
+            variant: 'caption',
+            color: 'text.secondary',
+          }}
+          secondary={data.github}
+          secondaryTypographyProps={{
+            variant: 'body2',
+            noWrap: true,
+            textOverflow: 'ellipsis',
+            color: 'text.primary',
+          }}
+        />
+      </ListItem>
+      <ListItem disablePadding divider>
+        <ListItemText
+          primary={t('application:Information.AgeRating')}
+          primaryTypographyProps={{
+            variant: 'caption',
+            color: 'text.secondary',
+          }}
+          secondary={data.ageRating}
+          secondaryTypographyProps={{
+            variant: 'body2',
+            noWrap: true,
+            textOverflow: 'ellipsis',
+            color: 'text.primary',
+          }}
+        />
+      </ListItem>
+      <ListItem disablePadding divider>
+        <ListItemText
+          primary={t('application:Information.Compatibility')}
+          primaryTypographyProps={{
+            variant: 'caption',
+            color: 'text.secondary',
+          }}
+          secondary={
+            <Stack gap={1}>
+              {data.compatibility.map((item) => (
+                <Typography
+                  key={item.platform}
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {item.requirement}
+                </Typography>
+              ))}
+            </Stack>
+          }
+          secondaryTypographyProps={{
+            component: 'div',
+          }}
+        />
+      </ListItem>
+      <ListItem disablePadding divider>
+        <ListItemText
+          primary={t('application:Information.Countries')}
+          primaryTypographyProps={{
+            variant: 'caption',
+            color: 'text.secondary',
+          }}
+          secondary={
+            <Stack direction={'row'} gap={1} flexWrap={'wrap'}>
+              {data.countries.map((item) => (
+                <Chip key={item} label={item} />
+              ))}
+            </Stack>
+          }
+          secondaryTypographyProps={{
+            component: 'div',
+          }}
+        />
+      </ListItem>
+      <ListItem disablePadding divider>
+        <ListItemText
+          primary={t('application:Information.Locales')}
+          primaryTypographyProps={{
+            variant: 'caption',
+            color: 'text.secondary',
+          }}
+          secondary={
+            <Stack direction={'row'} gap={1} flexWrap={'wrap'}>
+              {data.locales.map((item) => (
+                <Chip key={item} label={item} />
+              ))}
+            </Stack>
+          }
+          secondaryTypographyProps={{
+            component: 'div',
+          }}
+        />
+      </ListItem>
+    </List>
   );
 };
